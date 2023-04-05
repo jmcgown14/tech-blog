02f0aadb7942e7f36a4ffe9ca50f154260b99635
Johnny McGown <jmac3292@gmail.com>
2023-04-04 20:36:14 -0600

    Initial commit, updating mini project for challenge

---
+++ models/index.js
@@ -0,0 +1,21 @@
+const User = require('./user');
+const Post = require('./post');
+const Comment = require('./comment');
+
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+Post.hasMany(Comment, {
+    foreignKey: 'post_id',
+    onDelete: 'CASCADE'
+});
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+
+module.exports = { User, Comment, Post};
